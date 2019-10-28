efe83c4d4664ede8011d9b4cf2c9db7b2b0382d5
ibrahimothman <ibrahimothmanre@gmail.com>
2019-10-28 23:05:35 +0200

    initial commit

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,14 @@
+<ul class="nav py-3">
+    <li class="nav-item">
+        <a class="nav-link" href="">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="">About Us</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="">Contact Us</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/customers">Customer List</a>
+    </li>
+</ul>
